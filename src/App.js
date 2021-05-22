2178f132e81f2527773f547180f34a473adc7152
zcvaters <zachvaters@gmail.com>
2021-05-22 18:15:59 -0230

    bug: routing issue with path

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         <Header />
       </div>
       <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/blog" component={Blog} />
-        <Route path="/contact" component={Contact} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </div>
   );
