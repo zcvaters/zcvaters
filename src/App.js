a8ba8d9afe52ff40ec49fda9840aeb6141695e30
zcvaters <zachvaters@gmail.com>
2021-05-22 19:00:39 -0230

    add public url

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { Switch, Route, withRouter } from 'react-router-dom'
 
 import "./App.css"
 
+const { PUBLIC_URL } = process.env; 
+
 function App() {
   return (
     <div className="App">
       <div>
         <Header />
       </div>
+      <BrowserRouter basename={PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
@@ -17,6 +20,7 @@ function App() {
         <Route exact path="/blog" component={Blog} />
         <Route exact path="/contact" component={Contact} />
       </Switch>
+      </BrowserRouter>
     </div>
   );
 }
